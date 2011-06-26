5bdb7e8cb17a460e1abc6bf382e570a915f6a8ef
Taziff <satelita20@gmail.com>
2011-06-26 07:11:16 +0200

    Setting view optimization

--- settings.cpp
+++ settings.cpp
@@ -6,9 +6,7 @@ Settings::Settings(QWidget *parent) :
     ui(new Ui::Settings)
 {
     ui->setupUi(this);
-    connect(ui->z1OK, SIGNAL(clicked()), this, SLOT(hide()));
-    connect(ui->z2OK, SIGNAL(clicked()), this, SLOT(hide()));
-
+    connect(ui->cancelButton, SIGNAL(clicked()), this, SLOT(hide()));
 }
 
 Settings::~Settings()
